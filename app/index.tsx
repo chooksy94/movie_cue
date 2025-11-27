c9867cb275227534c1f1cdef50c838928f8247fe
chooksy94 <chooksyanumba2594@gmail.com>
2025-11-26 21:03:06 -0500

    deleted movie dynamic routing folder

--- app/index.tsx
+++ app/index.tsx
@@ -7,8 +7,6 @@ export default function Index() {
       <View className="flex-1 justify-center items-center">
       <Text className="text-4xl text-dark-200 font-bold"> Welcome! </Text>
 
-          <Link href="/movie/avengers">Avengers Movie </Link>
-
     </View>
   );
 }
