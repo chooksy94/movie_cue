90171e6b6c64a97788c1603f78944bc472bd33da
chooksy94 <chooksyanumba2594@gmail.com>
2025-11-30 21:06:22 -0500

    updated file (tabs)/_layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,37 +5,10 @@ import {Tabs} from "expo-router";
 const _TabLayout = () => {
     return (
         <Tabs>
-            <Tabs.Screen
-                name="index"
-                options={{
-                    title: 'Home',
-                    headerShown: false
-                }}
-            />
-
-            <Tabs.Screen
-                name="search"
-                options={{
-                    title: 'Search',
-                    headerShown: false
-                }}
-            />
-
-            <Tabs.Screen
-                name="saved"
-                options={{
-                    title: 'Saved',
-                    headerShown: false
-                }}
-            />
-
-            <Tabs.Screen
-                name="profile"
-                options={{
-                    title: 'Profile',
-                    headerShown: false
-                }}
-            />
+            <Tabs.Screen name="index" options={{title: 'Home', headerShown: false}}/>
+            <Tabs.Screen name="search" options={{title: 'Search', headerShown: false}}/>
+            <Tabs.Screen name="saved" options={{title: 'Saved', headerShown: false}}/>
+            <Tabs.Screen name="profile" options={{title: 'Profile', headerShown: false}}/>
         </Tabs>
     )
 }
