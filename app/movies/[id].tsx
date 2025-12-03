ec4a5499c1ad2f34d1ec601dae7540559ef3497a
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-02 22:41:38 -0500

    Updated file [id].tsx

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native"
 import React from 'react'
-
+/* The movie details and ratings will be modified in this fileexit */
 const MovieDetails = () => {
     return (
         <View>
