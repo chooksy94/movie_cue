8e2c71820d021e7b96ed6465dc7cc9dcec939601
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-03 22:55:34 -0500

    Updated Search Tab Screen icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,19 +34,20 @@ const _TabLayout = () => {
                     title: 'Home',
                     headerShown: false,
                     tabBarIcon: ({focused}) => (
-                        <>
-                            <ImageBackground
-                            source={images.highlight}
-                            className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-                            >
-                                <Image source={icons.home} tintColor="#151312" className="size-5"/>
-                                <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
-                            </ImageBackground>
-                        </>
+                      <TabIcon/>
                     )
             }}
             />
-            <Tabs.Screen name="search" options={{title: 'Search', headerShown: false}}/>
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: 'Search',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon/>
+                    )
+            }}
+            />
             <Tabs.Screen name="saved" options={{title: 'Saved', headerShown: false}}/>
             <Tabs.Screen name="profile" options={{title: 'Profile', headerShown: false}}/>
 
