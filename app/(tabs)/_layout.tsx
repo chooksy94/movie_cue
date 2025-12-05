afa2f4b9952d720c2ea38408a8b8ec3db9e3fa3e
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-04 21:31:41 -0500

    Update file(tabs)/_layout.tsx<TabIcon/>: extracted and assigned props for focused,title, icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,19 +5,19 @@ import {Image, ImageBackground, Text} from "react-native";
 import {icons} from "@/constants/icons";
 
 /*Creating a prop to pass down similar modification components from the parent function down to each root Tabs Icon */
-const TabIcon = () => {
+const TabIcon = ({ focused, icon, title }: any) => {
     return (
         <ImageBackground
             source={images.highlight}
             className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
         >
             <Image
-                source={icons.home}
+                source={icon}
                 tintColor="#151312"
                 className="size-5"
             />
             <Text className="text-secondary text-base font-semibold ml-2">
-                Home
+                {title}
             </Text>
         </ImageBackground>
     );
