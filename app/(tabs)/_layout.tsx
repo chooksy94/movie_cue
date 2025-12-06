eb10a9b340312bbb5b6232ff8ed411aab6330a94
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-05 20:26:01 -0500

    Updated Tab Bar styling and background color hen focused

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,12 +5,12 @@ import {Image, ImageBackground, Text, View} from "react-native";
 import {icons} from "@/constants/icons";
 
 /*Creating a prop to pass down similar modification components from the parent function down to each root Tabs Icon */
-const TabIcon = ({focused, icon , title }: any) => {
+const TabIcon = ({focused, icon , title }: any) =>  {
     if (focused) {
     return (
         <ImageBackground
             source={images.highlight}
-            className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+            className="flex flex-row w-full flex-1 min-w-[112px] min-h-16  mt-4 justify-center items-center rounded-full overflow-hidden"
         >
             <Image
                 source={icon}
@@ -21,7 +21,7 @@ const TabIcon = ({focused, icon , title }: any) => {
             </ImageBackground>
         )
     }
-
+/*To make the tab bar icons visible*/
     return (
         <View className="size-full justify-center item-center mt-4 rounded-full ">
             <Image source={icon} tintColor="AB8B5DB" className="size-5"/>
@@ -33,7 +33,32 @@ const TabIcon = ({focused, icon , title }: any) => {
 /* The root tab screen will be modified in the tabs created below*/
 const _TabLayout = () => {
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    width: '100%',
+                    height: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                },
+
+                tabBarStyle: {
+                    backgroundColor: '#0f0D23',
+                    borderRadius: 50,
+                    marginHorizontal: 20,
+                    marginBottom: 36,
+                    height: 52,
+                    position: 'absolute',
+                    overflow: 'hidden',
+                     borderWidth: 1,
+                    borderColor: '#0f0D23',
+
+                }
+        }}
+        >
+
             <Tabs.Screen
                 name="index"
                 options={{
