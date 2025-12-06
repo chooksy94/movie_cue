03f682ff102625f6e357a471762337291e2c2a12
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-05 23:15:35 -0500

    Updated file app/(tabs)/index.tsx: added a hidden VerticalScrollIndicator for my app screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,9 @@ export default function Index() {
       <View className="flex-1 bg-primary">
           <Image source={images.bg} className="absolute w-full z-0" />
 
-          <ScrollView className={"flex-1 px-5"}>
+          <ScrollView className="flex-1 px-5"
+                      showsVerticalScrollIndicator={false} contentContainerStyle={{
+                          minHeight: "100%", paddingBottom: 10}}>
               <Image source={icons.logo} className="absolute w-12 h-10 mt-20 mb-5 mx-auto" />
 
           </ScrollView>
