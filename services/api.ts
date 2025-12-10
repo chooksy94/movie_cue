aec2d852ec2d704aa25feefe2d22ca1a7fa6b0c4
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-09 22:07:12 -0500

    Added a .env file in root directory: It will be used to save our API key

--- services/api.ts
+++ services/api.ts
@@ -1,15 +1,15 @@
-/*Handling API request*/
-
-const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGJiNmQ0NmQ5MTc2MzMwY2ZkNTQ1NTQ1NDhmOWEwMCIsIm5iZiI6MTc2NTMzMzM4OS43NDEsInN1YiI6IjY5MzhkOThkNGNkMWY3MmM0MmY4NzgyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CU7TJnuXnGKAmNNc3oTxKkFPrTp6kIh9eNO1b-rFjxI'
-    }
-};
-
-fetch(url, options)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.error(err));
+// /*Handling API request*/
+//
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGJiNmQ0NmQ5MTc2MzMwY2ZkNTQ1NTQ1NDhmOWEwMCIsIm5iZiI6MTc2NTMzMzM4OS43NDEsInN1YiI6IjY5MzhkOThkNGNkMWY3MmM0MmY4NzgyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CU7TJnuXnGKAmNNc3oTxKkFPrTp6kIh9eNO1b-rFjxI'
+//     }
+// };
+//
+// fetch(url, options)
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+//     .catch(err => console.error(err));
