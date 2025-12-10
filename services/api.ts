68d173d973e26d9b89b4f942b69d7622cb80dbe1
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-09 22:17:27 -0500

    Updated file services/api.ts: Added a Base URl and API key to the TMDB API configuration

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,14 @@
 // /*Handling API request*/
-//
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+    }
+}
+
+
 // const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
 // const options = {
 //     method: 'GET',
