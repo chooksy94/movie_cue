4f919c2db659c1651f93bcfbcdafc3dc2a0c26a4
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-12 18:34:13 -0500

    Updated file usefetch.ts

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -3,11 +3,13 @@ import {useState} from "react";
 const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
     const [data, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState‹Error | null>(null);
+    const [error, setError] = useState<Error | null>(null);
+
     const fetchData = async () => {
         try {
 
         } catch (err) {
+            //@ts-gnore
             setError(err instanceof Error ? err : new Error('An error occurred'));
         }
     }
