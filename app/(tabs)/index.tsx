b6fd76a584798ca0e91f7ce3da09e5662a04546e
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-17 19:00:08 -0500

    Updated file: index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -43,7 +43,7 @@ export default function Index() {
                       />
 
                       <>
-                        <Text className="text-lg text-white font-bold mt-5 mb-3"> New Movies </Text>
+                        <Text className="text-lg text-white font-bold mt-5 mb-3"> Latest Movies </Text>
                       </>
                   </View>
               )}
