0559c1142397b261297b6ed68a5a3e6ab5f6cc7b
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-18 19:41:45 -0500

    Updated file MovieCard.tsx: Added Vote_average for movies

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import {View, Text, TouchableOpacity} from "react-native"
 import {Link} from "expo-router";
 import {Image} from "react-native";
+import { icons } from "@/constants/icons";
 
 
 const MovieCard = ({ id, poster_path, title, vote_average,  release_date }: Movie ) => {
@@ -16,8 +17,11 @@ const MovieCard = ({ id, poster_path, title, vote_average,  release_date }: Movi
                 className="w-full h-52 rounded-lg"
                 resizeMode="cover"
              />
-
                 <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+                <View className="flex-row items-center justify-start gap-x-1">
+                    <Image src={icons.star} className="size-4" />
+                    <Text className="text-xs text-white font-bold uppercase">{Math.round(vote_average / 2)}</Text>
+                </View>
             </TouchableOpacity>
         </Link>
     )
