76a3aca5f9640e98026bacbce023393398002665
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-20 16:26:28 -0500

    Updated file MovieCard.tsx>> used numberOfLines hook to display movie title in one line

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -17,7 +17,7 @@ const MovieCard = ({ id, poster_path, title, vote_average,  release_date }: Movi
                 className="w-full h-52 rounded-lg"
                 resizeMode="cover"
              />
-                <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+                <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>{title}</Text>
                 <View className="flex-row items-center justify-start gap-x-1">
                     <Image src={icons.star} className="size-4" />
                     <Text className="text-xs text-white font-bold uppercase">{Math.round(vote_average / 2)}</Text>
