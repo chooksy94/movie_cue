32303fe6df4df2d29e6276ffc003e2d066767209
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-20 16:59:01 -0500

    Updated file search.tsx: styled the background for search screen and imported a flatlist for calling movies

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,27 @@
-import { View, Text } from "react-native"
-import React from 'react'
+import { images } from "@/constants/images"
+import {View, Text, Image, FlatList} from "react-native"
+import {useRouter} from "expo-router";
+import useFetch from "@/services/useFetch";
+import {fetchMovies} from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 const Search = () => {
+    const router = useRouter();
+// fetching movie data
+    const { data : movies,
+        loading : moviesLoading,
+        error : moviesError } = useFetch(() => fetchMovies({
+        query: ''
+    }))
     return (
-        <View>
-            <Text>Search</Text>
+ //styling the background of search screen
+        <View className="flex-1 bg-primary">
+            <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover"/>
+            
+            <FlatList
+                data={movies}
+                renderItem={({item}) => <MovieCard {...item}/>}
+            />
         </View>
     )
 }
