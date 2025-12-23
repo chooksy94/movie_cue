64f6be42f71ced0f4812708dc803008163e38a3a
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-22 23:04:27 -0500

    Updated file: SearchBar.tsx>> implemented value prop

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,16 +6,17 @@ interface Props {
     placeholder: string;
     onPress?: () => void;
     value: string;
+    onChangeText: () => void;
 }
-const SearchBar = ({placeholder, onPress, value}: Props) => {
+const SearchBar = ({placeholder, onPress, value, onChangeText}: Props) => {
     return (
         <View className="flex-row items-centerbg-dark-200 rounded-full px-5 py-4 " >
             <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
             <TextInput
             placeholder={placeholder}
             onPress={onPress}
-            value=""
-            onChangeText={() => {}}
+            value={value}
+            onChangeText={onChangeText }
             placeholderTextColor={"#a8b5db"}
             className="flex-1 ml-2 text-white"
             />
