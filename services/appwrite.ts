94a7b8c2e9193579ccfa972e9a8bb32064b8a03f
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-25 19:46:03 -0500

    UPDATED .ENV FILE

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,5 +1,7 @@
 // track searches made by users
 
+const DATABBASE_ID= process.env.EXPO_PUBLIC_APPWRITE_DATABBASE_ID;
+
 export const updateSearchCount = async (query : string, movie: Movie  ) => {
 
 }
