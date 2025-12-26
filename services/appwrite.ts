310419983d571f013c33d34334d3e31233b5acfe
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-25 19:57:35 -0500

    UPDATED .ENV FILE>> initialized the appwrite database by calling the client function

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,6 +1,6 @@
 // track searches made by users
 
-import {Client} from "react-native-appwrite";
+import {Client, Databases} from "react-native-appwrite";
 
 const DATABBASE_ID= process.env.EXPO_PUBLIC_APPWRITE_DATABBASE_ID!;
 const COLLECTION_ID= process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
@@ -8,6 +8,8 @@ const COLLECTION_ID= process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(process.env.EXPO_PUBLIC_APPWRITE_DATABBASE_ID!)
+
+const database = new Databases(client)
 export const updateSearchCount = async (query : string, movie: Movie  ) => {
 
 }
