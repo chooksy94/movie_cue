a4f104ba2871891cedce11fe7d58803f406b4b89
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-25 19:53:42 -0500

    UPDATED .ENV FILE>> set endpoint for calling appwrite API

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,8 +1,13 @@
 // track searches made by users
 
+import {Client} from "react-native-appwrite";
+
 const DATABBASE_ID= process.env.EXPO_PUBLIC_APPWRITE_DATABBASE_ID!;
 const COLLECTION_ID= process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_DATABBASE_ID!)
 export const updateSearchCount = async (query : string, movie: Movie  ) => {
 
 }
