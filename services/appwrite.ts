969a8e676baa350d5c6316a452c3cdeec0977222
chooksy94 <chooksyanumba2594@gmail.com>
2025-12-30 20:43:15 -0500

    updated file index.tsx>> added missing type for calling the SearchBar function

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -5,6 +5,8 @@ import {Client, Databases, ID, Query} from "react-native-appwrite";
 const DATABASE_ID= process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID= process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
+
+
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
@@ -33,6 +35,7 @@ export const updateSearchCount = async (query : string, movie: Movie, any = resu
             await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
                 searchTerm: query,
                 movie_id: movie.id,
+                title: movie.title,
                 count: 1,
                 poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
             })
@@ -44,6 +47,7 @@ export const updateSearchCount = async (query : string, movie: Movie, any = resu
     }
 
 console.log(result);
+
 
 }
 
